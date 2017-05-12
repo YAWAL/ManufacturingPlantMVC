359419ae4a8d55928615c7a1a637fc92745c408b
YAWAL <yavorskyyval@gmail.com>
2017-05-12 20:26:56 +0300

    initial API for employees DAO layer

--- src/main/java/com/manufacturingPlant/virtualPlant/dao/api/DepartmentEmployeeDAO.java
+++ src/main/java/com/manufacturingPlant/virtualPlant/dao/api/DepartmentEmployeeDAO.java
@@ -1,7 +1,22 @@
 package com.manufacturingPlant.virtualPlant.dao.api;
 
+import com.manufacturingPlant.virtualPlant.model.DepartmentEmployee;
+
+import java.util.List;
+
 /**
  * Created by VYA on 11.05.2017.
  */
 public interface DepartmentEmployeeDAO {
+
+    public void addDepartmentEmployee(DepartmentEmployee departmentEmployee);
+
+    public void updateDepartmentEmployee(DepartmentEmployee departmentEmployee);
+
+    public List<DepartmentEmployee> showAllDepartmentEmployees();
+
+    public List<DepartmentEmployee> showDepartmentEmployeesByDepartment(String departmentName);
+
+    public void deleteDepartmentEmployee(DepartmentEmployee departmentEmployee);
+
 }
