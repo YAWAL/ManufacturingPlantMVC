085b173fafe42dffb89754dbab7f147a2eb1678e
YAWAL <yavorskyyval@gmail.com>
2017-05-23 22:40:06 +0300

    developing new classes

---
+++ src/main/java/com/manufacturingPlant/virtualPlant/model/Tool.java
@@ -0,0 +1,7 @@
+package com.manufacturingPlant.virtualPlant.model;
+
+/**
+ * Created by VYA on 23.05.2017.
+ */
+public class Tool {
+}
--- src/main/java/com/manufacturingPlant/virtualPlant/model/Workshop.java
+++ src/main/java/com/manufacturingPlant/virtualPlant/model/Workshop.java
@@ -1,32 +1,43 @@
 package com.manufacturingPlant.virtualPlant.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.ArrayList;
 
 @Entity
 @Table(name = "workshops")
 public class Workshop {
 
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @Column(name = "workshop_id")
     private String workshopId;
 
+    @Column
     private String workshopName;
 
+    @Column
     private String chairmanEmployeeId; // FK to workshop_employees 'one-to-one'
 
+    @Column
     private ArrayList<WorkshopEmployee> workshopEmployees;//FK to workshop_employees 'many-to-one'
 
+    @Column
     private int workshopEmployeeQuantity;// do not include to constructor
 
-//    private ArrayList<ProductionEquipment> productionEquipment;
+    @Column
+    private ArrayList<ProductionEquipment> productionEquipment;
 
-//    private ArrayList<NonProductionEquipment> nonProductionEquipment;
+    @Column
+    private ArrayList<NonProductionEquipment> nonProductionEquipment;
 
-//    private ArrayList<Tool> tools;
-//
-//    private ArrayList<Material> materials;
+    @Column
+    private ArrayList<Tool> tools;
+
+    @Column
+    private ArrayList<Material> materials;
 
 
 
