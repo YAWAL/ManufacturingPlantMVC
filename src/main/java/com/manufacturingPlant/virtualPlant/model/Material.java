4e29cf79f2a99e995001bccc954797ba6ce31e58
YAWAL <yavorskyyval@gmail.com>
2017-05-25 22:08:51 +0300

    developing classes

--- src/main/java/com/manufacturingPlant/virtualPlant/model/Material.java
+++ src/main/java/com/manufacturingPlant/virtualPlant/model/Material.java
@@ -6,4 +6,20 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "materials")
 public class Material {
+
+    private int id;
+
+    private String name;
+
+    private String description;
+
+    private String standard;
+
+    private String primarySize;
+
+    private String productionYear;
+
+    private String manufacturerName;
+
+    private String vendorName;
 }
