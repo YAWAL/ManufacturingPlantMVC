37b9b7fa1385d3d13a2d5480c35134aaae54d84d
YAWAL <yavorskyyval@gmail.com>
2017-05-25 23:22:21 +0300

    developing classes

--- src/main/java/com/manufacturingPlant/virtualPlant/model/Tool.java
+++ src/main/java/com/manufacturingPlant/virtualPlant/model/Tool.java
@@ -2,8 +2,31 @@ package com.manufacturingPlant.virtualPlant.model;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "tools")
 public class Tool {
+
+    private int id;
+
+    private String inventoryNum;
+
+    private String name;
+
+    private String description;
+
+    private String workshopName; // FR to workshops
+
+    private LocalDate startDate;
+
+    private String productionYear; // or LocalDate
+
+    private String manufacturerName;
+
+    private String vendorName;
+
+    private int price;
+
+
 }
