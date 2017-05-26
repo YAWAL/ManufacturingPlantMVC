8a72f72ba43f825713a52eed8d05879d8045ff9c
YAWAL <yavorskyyval@gmail.com>
2017-05-26 18:12:07 +0300

    developing classes

--- src/main/java/com/manufacturingPlant/virtualPlant/model/Department.java
+++ src/main/java/com/manufacturingPlant/virtualPlant/model/Department.java
@@ -1,23 +1,30 @@
 package com.manufacturingPlant.virtualPlant.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.ArrayList;
 
 @Entity
 @Table(name = "departments")
 public class Department {
 
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @Column
     private String departmentId;
 
+    @Column
     private String departmentName;
 
+    @Column
     private String chairmanEmployeeId; // FK to workshop_employees 'one-to-one'
 
+    @Column
     private ArrayList<DepartmentEmployee> departmentEmployees;//FK to workshop_employees 'many-to-one'
 
+    @Column
     private int departmentEmployeeQuantity;// do not include to constructor
 
 //    private ArrayList<NonProductionEquipment> nonProductionEquipment;
--- src/main/java/com/manufacturingPlant/virtualPlant/model/Material.java
+++ src/main/java/com/manufacturingPlant/virtualPlant/model/Material.java
@@ -1,25 +1,34 @@
 package com.manufacturingPlant.virtualPlant.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "materials")
 public class Material {
 
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @Column
     private String name;
 
+    @Column
     private String description;
 
+    @Column
     private String standard;
 
+    @Column
     private String primarySize;
 
+    @Column
     private String productionYear;
 
+    @Column
     private String manufacturerName;
 
+    @Column
     private String vendorName;
 }
