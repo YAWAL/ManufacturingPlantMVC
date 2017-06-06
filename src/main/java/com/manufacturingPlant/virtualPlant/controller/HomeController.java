4550e21890ebc38cd7060faf3ff8851f7c0e20c4
YAWAL <yavorskyyval@gmail.com>
2017-06-06 18:57:35 +0300

    implemented simple HomeController for checking

--- src/main/java/com/manufacturingPlant/virtualPlant/controller/HomeController.java
+++ src/main/java/com/manufacturingPlant/virtualPlant/controller/HomeController.java
@@ -1,29 +1,53 @@
 package com.manufacturingPlant.virtualPlant.controller;
 
+import com.manufacturingPlant.virtualPlant.service.api.DepartmentEmployeeService;
+import com.manufacturingPlant.virtualPlant.service.api.EmployeeService;
+import com.manufacturingPlant.virtualPlant.service.api.WorkshopEmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping(value = "/")
+    private final DepartmentEmployeeService departmentEmployeeService;
+    private final EmployeeService employeeService;
+    private final WorkshopEmployeeService workshopEmployeeService;
+
+    @Autowired
+    public HomeController(DepartmentEmployeeService departmentEmployeeService, EmployeeService employeeService,
+                          WorkshopEmployeeService workshopEmployeeService) {
+        this.departmentEmployeeService = departmentEmployeeService;
+        this.employeeService = employeeService;
+        this.workshopEmployeeService = workshopEmployeeService;
+    }
+
+    @RequestMapping(value = {"/", "/home"}, method = RequestMethod.GET)
     public String home(){
         return "home";
     }
 
+//    @RequestMapping(value = {"/", "/home"}, method = RequestMethod.GET)
+//    public String home(HttpSession httpSession, Principal principal){
+//        httpSession.setAttribute("user", userService.getUserById(Integer.valueOf(principal.getName())));
+//        return "home";
+//    }
+
+
     @RequestMapping(value = "/views/employees/employees.jsp")
     public String employees(){
-        return "employees";
+        return "/employees/employees";
     }
 
     @RequestMapping(value = "/views/employees/workshopEmployees.jsp")
     public String workshopEmployees(){
-        return "workshopEmployees";
+        return "employees/workshopEmployees";
     }
 
     @RequestMapping(value = "/views/employees/departmentEmployees.jsp")
     public String departmentEmployees(){
-        return "departmenEmployees";
+        return "/employees/departmentEmployees";
     }
 
 }
