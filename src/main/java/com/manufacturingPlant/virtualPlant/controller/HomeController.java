06650de4bd25b4d4a2af615b084d96fbc57bab05
YAWAL <yavorskyyval@gmail.com>
2017-06-11 02:01:17 +0300

    added logger

--- src/main/java/com/manufacturingPlant/virtualPlant/controller/HomeController.java
+++ src/main/java/com/manufacturingPlant/virtualPlant/controller/HomeController.java
@@ -7,11 +7,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpSession;
 import java.security.Principal;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 @Controller
 public class HomeController {
@@ -30,9 +34,11 @@ public class HomeController {
         this.workshopEmployeeService = workshopEmployeeService;
     }
 
-    @RequestMapping(value = {"/", "/home"}, method = RequestMethod.GET)
-    public String home(HttpSession httpSession, Principal principal){
 
+
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home(Locale locale, Model model){
         return "home";
     }
 
